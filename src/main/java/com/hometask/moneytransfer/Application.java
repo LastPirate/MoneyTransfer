41bc490805cd25fa5701fc5c5d5601f081e2133e
Ilya Mokin <mokin.ilya97@gmail.com>
2019-10-13 19:31:32 +0500

    Update schemas

--- src/main/java/com/hometask/moneytransfer/Application.java
+++ src/main/java/com/hometask/moneytransfer/Application.java
@@ -5,12 +5,14 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.name.Names;
 import com.hometask.moneytransfer.controller.TransferSystemController;
+import com.hometask.moneytransfer.model.db.tables.Account;
 import com.hometask.moneytransfer.service.TransferSystemService;
 import com.hometask.moneytransfer.service.TransferSystemServiceImpl;
 import org.jooq.Configuration;
 import org.jooq.DSLContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
+import org.jooq.impl.SQLDataType;
 
 import java.sql.DriverManager;
 
@@ -21,26 +23,20 @@ public class Application extends AbstractModule {
     private static final String URL = "jdbc:h2:file:C:\\Users\\First\\Desktop\\money_transfer";
 
     public static void main(String[] args) {
-        try {
-            Injector injector = Guice.createInjector(new Application());
-            injector.getInstance(TransferSystemController.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Injector injector = Guice.createInjector(new Application());
+        injector.getInstance(TransferSystemController.class);
     }
 
     @Override
     protected void configure() {
         try {
             DSLContext dslContext = DSL.using(DriverManager.getConnection(URL, USER_NAME, PASSWORD), SQLDialect.H2);
-
             bind(Configuration.class)
                     .annotatedWith(Names.named("DataBaseConfiguration"))
                     .toInstance(dslContext.configuration());
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         bind(TransferSystemService.class).to(TransferSystemServiceImpl.class);
     }
 }
